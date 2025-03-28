a14a5cb0c8320aea5b7a3b54c0f05f8ca204929c
Anirudh Phophalia <aphophalia_be24@thapar.edu>
2025-03-28 17:08:04 +0530

    Update config.py

--- quiz_master_24f2003068/app/config.py
+++ quiz_master_24f2003068/app/config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '24f2003068'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('quiz_master.db') or 'sqlite:///../quiz_master.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=7)
